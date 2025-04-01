9ed20701958f50780433a7e1a8722c1abe652636
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-04-01 22:23:04 +0200

    migrations work not finished yet

--- Backend/LogicPersistence/LogicPersistence.Api/Migrations/202503281_InitialMigration.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Migrations/202503281_InitialMigration.cs
@@ -8,6 +8,12 @@ public class InitialMigration : Migration {
 	public override void Up() {
 		Execute.Sql("CREATE SCHEMA IF NOT EXISTS public;");
 
+		Execute.Sql("CREATE TYPE IF NOT EXISTS hazard_level AS ENUM('unknown', 'low', 'medium', 'high', 'critical')");
+		Execute.Sql("CREATE TYPE IF NOT EXISTS physical_donation_type AS ENUM('other', 'food', 'tools', 'clothes', 'medicine', 'furniture')");
+		Execute.Sql("CREATE TYPE IF NOT EXISTS currency AS ENUM('other', 'usd', 'eur')");
+		Execute.Sql("CREATE TYPE IF NOT EXISTS payment_status AS ENUM('pending', 'completed', 'failed', 'refunded')");
+		Execute.Sql("CREATE TYPE IF NOT EXISTS payment_service AS ENUM('paypal', 'bank_transfer', 'credit_card', 'other')");
+
 		Create.Table("victim")
 			.WithColumn("id").AsInt32().PrimaryKey().Identity()
 			.WithColumn("email").AsString(50).NotNullable()
@@ -15,9 +21,9 @@ public class InitialMigration : Migration {
 			.WithColumn("name").AsString(50).NotNullable()
 			.WithColumn("surname").AsString(50).NotNullable()
 			.WithColumn("prefix").AsInt32().NotNullable()
-			.WithColumn("phone").AsInt32().NotNullable()
+			.WithColumn("phone_number").AsInt32().NotNullable()
 			.WithColumn("address").AsString(100).NotNullable()
-			.WithColumn("dni").AsString(20).NotNullable();
+			.WithColumn("identification").AsString(20).NotNullable();
 
 		Create.Table("volunteer")
 			.WithColumn("id").AsInt32().PrimaryKey().Identity()
@@ -26,12 +32,49 @@ public class InitialMigration : Migration {
 			.WithColumn("name").AsString(50).NotNullable()
 			.WithColumn("surname").AsString(50).NotNullable()
 			.WithColumn("prefix").AsInt32().NotNullable()
-			.WithColumn("phone").AsInt32().NotNullable()
+			.WithColumn("phone_number").AsInt32().NotNullable()
 			.WithColumn("address").AsString(100).NotNullable()
-			.WithColumn("dni").AsString(20).NotNullable();
+			.WithColumn("identification").AsString(20).NotNullable();
 
 		Create.Table("admin")
-			.WithColumn("jurisdiction").AsString(100).NotNullable();
+			.WithColumn("id").AsInt32().PrimaryKey().Identity()
+			.WithColumn("jurisdiction").AsString(100).NotNullable()
+			.WithColumn("email").AsString(50).NotNullable()
+			.WithColumn("password").AsString(50).NotNullable()
+			.WithColumn("name").AsString(50).NotNullable()
+			.WithColumn("surname").AsString(50).NotNullable()
+			.WithColumn("prefix").AsInt32().NotNullable()
+			.WithColumn("phone_number").AsInt32().NotNullable()
+			.WithColumn("address").AsString(100).NotNullable()
+			.WithColumn("identification").AsString(20).NotNullable();
+
+		Create.Table("affected_zone")
+			.WithColumn("id").AsInt32().PrimaryKey().Identity()
+			.WithColumn("name").AsString(50).NotNullable()
+			.WithColumn("description").AsString(3000).NotNullable()
+			.WithColumn("hazard_level").AsCustom("hazard_level").NotNullable().WithDefaultValue("unknown");
+
+		Create.Table("donation")
+			.WithColumn("id").AsInt32().PrimaryKey().Identity()
+			.WithColumn("donation_date").AsDateTime().NotNullable();
+
+		Create.Table("physical_donation")
+			.WithColumn("item_name").AsString(50).NotNullable()
+			.WithColumn("description").AsString(3000).NotNullable()
+			.WithColumn("quantity").AsInt32().NotNullable()
+			.WithColumn("physical_donation_type").AsCustom("physical_donation_type").NotNullable().WithDefaultValue("other");
+
+		Create.Table("monetary_donation")
+			.WithColumn("amount").AsInt32().NotNullable()
+			.WithColumn("item_name").AsString(50).NotNullable()
+			.WithColumn("description").AsString(3000).NotNullable()
+			.WithColumn("quantity").AsInt16().NotNullable()
+			.WithColumn("physical_donation_type").AsCustom("physical_donation_type").NotNullable().WithDefaultValue("other");
+
+		/* Foreign Key Example
+		Create.ForeignKey("FK_HelpRequest_Victim")
+        	.FromTable("help_request").ForeignColumn("victim_id")
+        	.ToTable("victim").PrimaryColumn("id");*/
 	}
 
 	public override void Down() {
--- Backend/LogicPersistence/LogicPersistence.Api/Models/Donation.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Models/Donation.cs
@@ -3,80 +3,78 @@ using System.ComponentModel.DataAnnotations;
 namespace LogicPersistence.Api.Models;
 
 public enum PhysicalDonationType {
-    [Display(Name = "Otro")]
-    Other,
-    [Display(Name = "Comida")]
-    Food,
-    [Display(Name = "Herramientas")]
-    Tools,
-    [Display(Name = "Ropa")]
-    Clothes,
-    [Display(Name = "Medicamentos")]
-    Medicine,
-    [Display(Name = "Muebles")]
-    Furniture
+	[Display(Name = "Otro")]
+	Other,
+	[Display(Name = "Comida")]
+	Food,
+	[Display(Name = "Herramientas")]
+	Tools,
+	[Display(Name = "Ropa")]
+	Clothes,
+	[Display(Name = "Medicamentos")]
+	Medicine,
+	[Display(Name = "Muebles")]
+	Furniture
 }
 
 public enum Currency {
-    [Display(Name = "Otro")]
-    Other,
-    [Display(Name = "Dólares")]
-    USD,
-    [Display(Name = "Euros")]
-    EUR
+	[Display(Name = "Otro")]
+	Other,
+	[Display(Name = "Dólares")]
+	USD,
+	[Display(Name = "Euros")]
+	EUR
 }
 
 public enum PaymentStatus {
-    [Display(Name = "Pendiente")]
-    Pending,
-    [Display(Name = "Completado")]
-    Completed,
-    [Display(Name = "Fallido")]
-    Failed,
-    [Display(Name = "Reembolsado")]
-    Refunded
+	[Display(Name = "Pendiente")]
+	Pending,
+	[Display(Name = "Completado")]
+	Completed,
+	[Display(Name = "Fallido")]
+	Failed,
+	[Display(Name = "Reembolsado")]
+	Refunded
 }
 
 public enum PaymentService {
-    [Display(Name = "PayPal")]
-    PayPal,
-    [Display(Name = "Stripe")]
-    Stripe,
-    [Display(Name = "Transferencia Bancaria")]
-    BankTransfer,
-    [Display(Name = "Tarjeta de Crédito")]
-    CreditCard,
-    [Display(Name = "Otro")]
-    Other
+	[Display(Name = "PayPal")]
+	PayPal,
+	[Display(Name = "Transferencia Bancaria")]
+	BankTransfer,
+	[Display(Name = "Tarjeta de Crédito")]
+	CreditCard,
+	[Display(Name = "Otro")]
+	Other
 }
 
 public class Donation {
-    public int id { get; set; }
-    public DateTime donation_date { get; set; }
+	public int id { get; set; }
+	public DateTime donation_date { get; set; }
 
-    // FKs
-    // donations can be done by volunteers or admins
-    public int volunteer_id { get; set; }
-    public int victim_id { get; set; }
-    public int admin_id { get; set; }
+	// FKs
+	// donations can be done by volunteers or admins
+	public int volunteer_id { get; set; }
+	public int victim_id { get; set; }
+	public int admin_id { get; set; }
 
-    //public virtual Volunteer Volunteer { get; set; }
-    //public virtual Victim Victim { get; set; }
-    //public virtual Admin Admin { get; set; }
-    //public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+	//public virtual Volunteer Volunteer { get; set; }
+	//public virtual Victim Victim { get; set; }
+	//public virtual Admin Admin { get; set; }
+	//public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
 }
 
 public class PhysicalDonation : Donation {
-    public string item_name { get; set; } = string.Empty;
-    public string description { get; set; } = string.Empty;
-    public int quantity { get; set; }
-    public PhysicalDonationType item_type { get; set; }
+	public string item_name { get; set; } = string.Empty;
+	public string description { get; set; } = string.Empty;
+	public int quantity { get; set; }
+	public PhysicalDonationType item_type { get; set; }
 }
 
 public class MonetaryDonation : Donation {
-    public decimal amount { get; set; }
-    public Currency currency { get; set; }
-    public PaymentStatus payment_status { get; set; }
-    public string transaction_id { get; set; } = string.Empty;
-    public PaymentService payment_service { get; set; }
+	public decimal amount { get; set; }
+	public Currency currency { get; set; }
+	public PaymentStatus payment_status { get; set; }
+	public string transaction_id { get; set; } = string.Empty;
+	public PaymentService payment_service { get; set; }
 }
