2285fd03f3221a12cb300e8b4e884d429f4ff37f
2004GAG <guille.albors@gmail.com>
2025-04-08 21:24:20 +0200

    Actualizacion auth_services
    Actualizo auth_services para añadir la demo para el registro con elementos básicos (gmail y contraseña), mañana en clase miramos cómo funciona la comunicación porque no entiendo nada más

--- App/lib/controllers/registerController.dart
+++ App/lib/controllers/registerController.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../services/auth_service.dart';
 
 class RegisterController {
   final emailController = TextEditingController();
@@ -9,13 +11,26 @@ class RegisterController {
     return passwordController.text == repeatPasswordController.text;
   }
 
-  void register() {
+  void register() async{
     final email = emailController.text.trim();
     final password = passwordController.text;
 
     print("Email: $email");
     print("Contraseña: $password");
-    print("Registro exitoso");
+    try {
+      final response = await AuthService.login(email, password); // Llamada al servicio
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print('Login exitoso');
+        print('Token recibido: ${data['token']}');
+      } else {
+        print('Error de login: ${response.statusCode}');
+        print('Mensaje: ${response.body}');
+      }
+    } catch (e) {
+      print('Error de conexión con el servidor: $e');
+    }
   }
 
   void dispose() {
--- App/lib/services/auth_service.dart
+++ App/lib/services/auth_service.dart
@@ -5,7 +5,7 @@ class AuthService {
   static Future<http.Response> login(String email, String password) async {
     final url = Uri.parse('https://tuservidor.com/api/login'); // ← Cambia esta URL por la tuya
 
-    final response = await http.post(
+    return await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
@@ -13,7 +13,18 @@ class AuthService {
         'password': password,
       }),
     );
+  }
 
-    return response;
+  static Future<http.Response> register(String email, String password) async {
+    final url = Uri.parse('https://tuservidor.com/api/register');
+
+    return await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
   }
 }
