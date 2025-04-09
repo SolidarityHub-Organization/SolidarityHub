c127c4413b3fe05b66a7dd1a033233e19f3c170e
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-04-09 13:49:53 +0200

    Create task functionality converted to modal

--- Backend/LogicPersistence/LogicPersistence.Api/Repositories/Interfaces/ILocationRepository.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Repositories/Interfaces/ILocationRepository.cs
@@ -2,10 +2,10 @@ using LogicPersistence.Api.Models;
 
 namespace LogicPersistence.Api.Repositories.Interfaces;
 
-public interface ILocationRepository
-{
-    Task<Location> CreateLocationAsync(Location location);
-    Task<Location> UpdateLocationAsync(Location location);
-    Task<bool> DeleteLocationAsync(int id);
-    Task<Location?> GetLocationByIdAsync(int id);
+public interface ILocationRepository {
+	Task<Location> CreateLocationAsync(Location location);
+	Task<Location> UpdateLocationAsync(Location location);
+	Task<bool> DeleteLocationAsync(int id);
+	Task<Location?> GetLocationByIdAsync(int id);
+	Task<IEnumerable<Location>> GetAllLocationsAsync();
 }
