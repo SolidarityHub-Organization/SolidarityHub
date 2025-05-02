ba38dd153e04d38e89df6098360a9b24442b3f38
el-kiosquet <obustosgimeno@gmail.com>
2025-05-02 12:05:36 +0200

    Now login backend returns name

--- Backend/LogicPersistence/LogicPersistence.Api/Services/LoginServices.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Services/LoginServices.cs
@@ -23,13 +23,16 @@ namespace LogicPersistence.Api.Services
             var victim = await _VictimRepository.GetVictimByEmailAsync(email);
             if (victim != null && victim.password == password)
             {
-                return System.Text.Json.JsonSerializer.Serialize(new { role = "victima", id = victim.id });
+                return System.Text.Json.JsonSerializer.Serialize(new { role = "victima", id = victim.id, name = victim.name });
             }
 			var volunteer = await _volunteerRepository.GetVolunteerByEmailAsync(email);
 			if(volunteer != null && volunteer.password == password) {
-				return System.Text.Json.JsonSerializer.Serialize(new { role = "voluntario", id = volunteer.id });
+				return System.Text.Json.JsonSerializer.Serialize(new { role = "voluntario", id = volunteer.id, name = volunteer.name });
 			}
-            if (victim != null || volunteer != null)
+            if (victim != null || volunteer != null) // Check if the email exists in either table
+            {
+                return System.Text.Json.JsonSerializer.Serialize(new { role = "password incorrect" });
+            }
             {
                 return System.Text.Json.JsonSerializer.Serialize(new { role = "exists" });
             }
