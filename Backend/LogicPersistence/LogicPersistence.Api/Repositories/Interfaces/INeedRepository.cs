d7feb97a6b5295134d35e2869d6cbf1b526752c0
Espartano2004 <127147782+Espartano2004@users.noreply.github.com>
2025-05-22 11:29:45 +0200

    Revert "Reapply "Need functionality""
    This reverts commit 12aa653fe0afe818185db84599a2351d5271c2e2.

--- Backend/LogicPersistence/LogicPersistence.Api/Repositories/Interfaces/INeedRepository.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Repositories/Interfaces/INeedRepository.cs
@@ -1,31 +1,25 @@
 using LogicPersistence.Api.Models;
-using LogicPersistence.Api.Models.DTOs;
 
-namespace LogicPersistence.Api.Repositories.Interfaces
-{
-    public interface INeedRepository
-    {
-#region Need
-        Task<Need> CreateNeedAsync(Need need);
-        Task<bool> DeleteNeedAsync(int id);
-        Task<Need?> GetNeedByIdAsync(int id);
-        Task<Need> UpdateNeedAsync(Need need);
-        Task<IEnumerable<Need>> GetAllNeedsAsync();
-		Task<IEnumerable<NeedWithVictimDetailsDto>> GetNeedWithVictimDetailsAsync(int id);
-		Task<Need> UpdateNeedStatusAsync(int id, string status);
-
+namespace LogicPersistence.Api.Repositories.Interfaces {
+	public interface INeedRepository {
+		#region Need
+		Task<Need> CreateNeedAsync(Need need);
+		Task<bool> DeleteNeedAsync(int id);
+		Task<Need?> GetNeedByIdAsync(int id);
+		Task<Need> UpdateNeedAsync(Need need);
+		Task<IEnumerable<Need>> GetAllNeedsAsync();
 		#endregion
 		#region NeedType
 		Task<NeedType> CreateNeedTypeAsync(NeedType needType);
-        Task<bool> DeleteNeedTypeAsync(int id);
-        Task<NeedType?> GetNeedTypeByIdAsync(int id);
-        Task<NeedType> UpdateNeedTypeAsync(NeedType needType);
-        Task<IEnumerable<NeedType>> GetAllNeedTypesAsync();
-        Task<int> GetVictimCountById(int id);
-        Task<Need?> GetNeedByVictimIdAsync(int id);
-        Task<int> GetVictimCountByIdFilteredByDate(int id, DateTime startDate, DateTime endDate);
-#endregion
+		Task<bool> DeleteNeedTypeAsync(int id);
+		Task<NeedType?> GetNeedTypeByIdAsync(int id);
+		Task<NeedType> UpdateNeedTypeAsync(NeedType needType);
+		Task<IEnumerable<NeedType>> GetAllNeedTypesAsync();
+		Task<int> GetVictimCountById(int id);
+		Task<Need?> GetNeedByVictimIdAsync(int id);
+		Task<int> GetVictimCountByIdFilteredByDate(int id, DateTime startDate, DateTime endDate);
+		#endregion
 
 
-    }
+	}
 }
