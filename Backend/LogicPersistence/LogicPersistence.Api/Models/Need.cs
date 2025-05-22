cef80da54e2a4070be31f841d1ef9e5b7174cf3c
Espartano2004 <127147782+Espartano2004@users.noreply.github.com>
2025-05-22 11:19:39 +0200

    Revert "Need functionality"
    This reverts commit b644ca22380aaf4898d265e2e21a7e52e7319c12.

--- Backend/LogicPersistence/LogicPersistence.Api/Models/Need.cs
+++ Backend/LogicPersistence/LogicPersistence.Api/Models/Need.cs
@@ -23,10 +23,10 @@ public class Need { // these specific needs that can be of a certain need type
 
 	public string description { get; set; } = string.Empty;
 
-	public UrgencyLevel urgencyLevel { get; set; }
+	public UrgencyLevel urgencyLevel { get; set; } = UrgencyLevel.Unknown;
 
-	public string status { get; set; } = string.Empty ;
-
+	public string status { get; set; } = "InProgress";
+	
 	// FKs
 	// a need can be set by an admin or a victim (one is null)
 	public int? victim_id { get; set; }
