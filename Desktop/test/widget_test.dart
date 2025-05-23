3757f957a774425252296b6da8ad4db6bccd1229
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-05-23 14:08:09 +0200

    dart testing implemented

--- Desktop/test/widget_test.dart
+++ Desktop/test/widget_test.dart
@@ -1,30 +1,72 @@
- // This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:solidarityhub/controllers/tasks/auto_assigner_controller.dart';
+import 'package:solidarityhub/models/volunteer.dart';
+import 'package:solidarityhub/models/skill.dart';
+import 'package:solidarityhub/models/location.dart';
+import 'package:solidarityhub/models/task.dart';
 
-import 'package:solidarityhub/main.dart';
+class MockVolunteer extends Volunteer {
+  MockVolunteer(int id, {super.skills = const [], super.location})
+    : super(
+        id: id,
+        email: '',
+        name: '',
+        surname: '',
+        prefix: 0,
+        phoneNumber: '',
+        address: '',
+        identification: '',
+        locationId: null,
+      );
+}
+
+class MockSkill extends Skill {
+  MockSkill(int id) : super(id: id, name: '');
+}
+
+class MockLocation extends Location {
+  MockLocation(double latitude, double longitude) : super(id: 0, latitude: latitude, longitude: longitude);
+}
+
+class MockTaskWithDetails extends TaskWithDetails {
+  MockTaskWithDetails({super.skills = const [], super.location, List<Volunteer>? assigned})
+    : super(
+        id: 0,
+        name: '',
+        description: '',
+        adminId: null,
+        locationId: 0,
+        startDate: DateTime.now(),
+        endDate: null,
+        assignedVolunteers: assigned ?? [],
+        assignedVictim: const [],
+      );
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('RandomAssignmentStrategy', () {
+    test('asigna voluntarios aleatoriamente a tareas', () {
+      final volunteers = List.generate(5, (i) => MockVolunteer(i));
+      final tasks = [MockTaskWithDetails(), MockTaskWithDetails()];
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      final strategy = RandomAssignmentStrategy();
+      final result = strategy.assignTasks(tasks as dynamic, volunteers as dynamic, 2);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(result[0].assignedVolunteers.length, 2);
+      expect(result[1].assignedVolunteers.length, 2);
+    });
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  group('BalancedAssignmentStrategy', () {
+    test('asigna voluntarios balanceando la carga', () {
+      final volunteers = List.generate(4, (i) => MockVolunteer(i));
+      final tasks = [MockTaskWithDetails(), MockTaskWithDetails(), MockTaskWithDetails()];
+
+      final strategy = BalancedAssignmentStrategy();
+      final result = strategy.assignTasks(tasks as dynamic, volunteers as dynamic, 1);
+
+      final totalAssignments = result.fold<int>(0, (sum, t) => sum + t.assignedVolunteers.length);
+      expect(totalAssignments, 3);
+    });
   });
 }
